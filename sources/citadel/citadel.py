db9ea9f0fe27c4f133660df3af0942d8a51b45f4
Adam Hammes <adam@hammes.io>
2016-02-14 15:11:22 -0600

    Add make_chapter, which returns a Chapter

--- sources/citadel/citadel.py
+++ sources/citadel/citadel.py
@@ -1,14 +1,16 @@
+from collections import namedtuple
 import requests
 from lxml import etree, html
 
 
 HOME_PAGE = 'https://unillustrated.wordpress.com/'
+Chapter = namedtuple('Chapter', ['title', 'text'])
 
 
 def generate_links():
     page = requests.get(HOME_PAGE)
     tree = html.fromstring(page.content)
-    for node in tree.cssselect("#linkcat-283635721 a"):
+    for node in tree.cssselect('#linkcat-283635721 a'):
         yield node.get('href')
 
 
@@ -16,7 +18,18 @@ def spanless(node):
     return all(child.tag != 'span' for child in node)
 
 
-def extract_content(html_string):
+def extract_content(tree):
+    nodes = filter(spanless, tree.cssselect('.entry-content p'))
+    raw_bytes = b''.join(map(etree.tostring, nodes))
+    return str(raw_bytes, encoding='UTF-8')
+
+
+def extract_title(tree):
+    return tree.cssselect('.entry-title')[0].text
+
+
+def make_chapter(html_string):
     tree = html.fromstring(html_string)
-    nodes = filter(spanless, tree.cssselect(".entry-content p"))
-    return b''.join(map(etree.tostring, nodes))
+    title = extract_title(tree)
+    text = extract_content(tree)
+    return Chapter(title, text)
