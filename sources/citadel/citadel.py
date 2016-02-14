6c91799e9306ec4f8a021c997e5d108d3c8ae0b9
Adam Hammes <adam@hammes.io>
2016-02-14 17:03:18 -0600

    Book creation

--- sources/citadel/citadel.py
+++ sources/citadel/citadel.py
@@ -1,14 +1,20 @@
+import time
 import requests
 from lxml import etree, html
 from ..tuples import Chapter
+from web import web
 
 HOME_PAGE = 'https://unillustrated.wordpress.com/'
+TITLE = 'Citadel: Training in Necessity'
+LINK_SELECTOR = '#linkcat-283635721 a'
+CONTENT_SELECTOR = '.entry-content p'
+TITLE_SELECTOR = '.entry-title'
 
 
 def generate_links():
     page = requests.get(HOME_PAGE)
     tree = html.fromstring(page.content)
-    for node in tree.cssselect('#linkcat-283635721 a'):
+    for node in tree.cssselect(LINK_SELECTOR):
         yield node.get('href')
 
 
@@ -17,13 +23,13 @@ def spanless(node):
 
 
 def extract_content(tree):
-    nodes = filter(spanless, tree.cssselect('.entry-content p'))
+    nodes = filter(spanless, tree.cssselect(CONTENT_SELECTOR))
     raw_bytes = b''.join(map(etree.tostring, nodes))
     return str(raw_bytes, encoding='UTF-8')
 
 
 def extract_title(tree):
-    return tree.cssselect('.entry-title')[0].text
+    return tree.cssselect(TITLE_SELECTOR)[0].text
 
 
 def make_chapter(html_string):
@@ -31,3 +37,18 @@ def make_chapter(html_string):
     title = extract_title(tree)
     text = extract_content(tree)
     return Chapter(title, text)
+
+
+def get_id():
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+    return TITLE + ' ' + time_str
+
+
+def get_metadata():
+    return {}
+
+
+def make_book():
+    pages = web.download_async(generate_links())
+    chapters = map(make_chapter, pages)
+    return Chapter(TITLE, get_id(), get_metadata(), chapters)
