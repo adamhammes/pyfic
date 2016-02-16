2ee91957ccbaaa88c255e0b419412ade1f1ec2af
Adam Hammes <adam@hammes.io>
2016-02-16 15:16:45 -0600

    Initial attempt

---
+++ epub.py
@@ -0,0 +1,26 @@
+from zipfile import ZipFile
+from sources.citadel import citadel
+from jinja2 import Environment, FileSystemLoader
+
+
+def main(env):
+    book = citadel.make_book()
+    with ZipFile('citadel.epub', 'w') as file:
+        file.write('templates/mimetype', 'mimetype')
+        file.write('templates/container.xml', 'META-INF/container.xml')
+
+        content_opf = env.get_template('content.jnj').render(book=book)
+        file.writestr('OEBPS/content.opf', content_opf)
+
+        tox_ncx = env.get_template('tox.jnj').render(book=book)
+        file.writestr('OEBPS/toc.ncx', tox_ncx)
+
+        for i, chapter in enumerate(book.chapters):
+            html = env.get_template('html.jnj').render(chapter=chapter)
+            file_name = 'OEBPS/content/Chapter{0}.html'.format(i + 1)
+            file.writestr(file_name, html)
+
+
+if __name__ == '__main__':
+    env = Environment(loader=FileSystemLoader('templates'))
+    main(env)
