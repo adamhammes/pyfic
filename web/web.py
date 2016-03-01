af650d9ad3dcbb956de043dd47e6db4c0859f2ee
Adam Hammes <adam@hammes.io>
2016-03-01 16:18:26 -0600

    Add documentation

--- web/web.py
+++ web/web.py
@@ -3,15 +3,22 @@ import asyncio
 
 
 async def get(session, url):
-    '''Coroutine for downloading a url'''
-    
+    """
+    A wrapper for a coroutine to asynchronously download a web page
+    :param session: The managing session
+    :param url: The link to dowload
+    :return: The html at the url given
+    """
     async with session.get(url) as response:
         return await response.text()
 
 
 def download_async(links):
-    '''Returns the html for each link (runs asynchronously)'''
-    
+    """
+    Download the given links asynchronously
+    :param links: An iterable containing the urls to download
+    :return: The html for each given link
+    """
     loop = asyncio.get_event_loop()
     with aiohttp.ClientSession(loop=loop) as session:
         tasks = (get(session, link) for link in links)
